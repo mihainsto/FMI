2d42fe7dda62e53e95c7ac5930a4a2d6cc3a9807
mihainsto <44275480+mihainsto@users.noreply.github.com>
2019-11-29 00:38:23 +0200

    Update sharedmemory.c

--- Year 2 Sem 1/so/lab5/sharedmemory.c
+++ Year 2 Sem 1/so/lab5/sharedmemory.c
@@ -1,84 +1,77 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <sys/shm.h>
-#include <sys/mman.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
+#include <errno.h>
 #include <sys/wait.h>
- 
-int main(int argc, char*argv[])
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+
+int collatz(int* buffer, int n) 
 {
-   int n = argc;
-   const int SIZE = n * 2048;
-   const char *name = "/COLLATZ";
-   int  shm_fd;
-   void *ptr;
-   int count = 1;
-   
-   /* Setup shared memory */
-   shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
-   ftruncate(shm_fd, SIZE);
-   ptr = mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
-   
-   for (int i=0 ;i<argc;i++)
-    {
- 
-       ptr += i*sizeof(int);
-       
-       int value = atoi(argv[i]);
- 
-       pid_t pid;
-       pid = fork();
- 
- 
-       if (pid < 0)
-       {
-          fprintf(stderr, "FORK FAILED\n");
-          return 1;
-       }
-       else if (pid > 0) // parent process
-       {
-          wait(NULL);
-          printf("\n");
-     
- 
-          while (atoi((char *)ptr) != 0)
-          {
-            printf("%s", (char *)ptr);
-            ptr += sizeof(int);
-            if (atoi((char *)ptr) != 0)
-                    printf(", ");
-          }
-       
-          printf("\n");
- 
-       }
-       else if (pid == 0)   // child process
-       {  
- 
-          sprintf(ptr, "%d", value);
-          ptr += sizeof(value);
- 
-          while (value != 1)
-          {
-            if (value % 2 == 0)
-              value /= 2;
-            else
-              value = value * 3 + 1;
- 
-            sprintf(ptr, "%d", value);
-            ptr += sizeof(value);
-          }
-          sprintf(ptr,"0");
-          ptr += sizeof(value);
-          return 0;
-       }
-    }
-   return 0;
+	buffer[0] = n;
+	int k = 1;
+	while (n != 1) 
+	{
+		if (n % 2 == 0) 
+			n = n / 2;
+		else 
+			n = 3 * n + 1;
+		buffer[k] = n;
+		k++;
+	}
+	return 0;
+}
+
+
+
+
+int main(int argc, char* argv[])
+{
+	char shm_name[] = "/collatz";
+	int shm_fd;
+
+	shm_fd = shm_open(shm_name, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+	size_t shm_size = 2048 * argc;
+	ftruncate(shm_fd, shm_size);
+	int* addr = mmap(0, shm_size, PROT_WRITE, MAP_SHARED, shm_fd, 0);
+
+	printf("Starting parent %d\n", getpid());
+
+	for (int i = 1; i < argc; i++) {
+		pid_t pid = fork();
+		int n = atoi(argv[i]);
+
+		if (pid < 0) {
+			printf("Fork failed");
+			return errno;
+		}
+		else if (pid == 0) {
+			printf("Done parent %d Me %d\n", getppid(), getpid());
+			int* buffer = addr + (i - 1) * 2048 / sizeof(int);
+			collatz(buffer, n);
+			return 0;
+		}
+	}
+
+	for (int i = 0; i < argc; i++) {
+		wait(NULL);
+	}
+
+	for (int i = 1; i < argc; i++) {
+		int*  buffer = addr + (i - 1) * 2048 / sizeof(int);
+		int j = 0;
+		printf("%d: ", buffer[j]);
+
+		while (buffer[j] != 1) {
+			printf("%d ", buffer[j]);
+			j++;
+		}
+		printf("\n");
+
+	}
+	shm_unlink("/collatz");
+	return 0;
 }
